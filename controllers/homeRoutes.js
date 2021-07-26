1383ab7150258c8c1fafc6e619aebbeabd7451e6
LemonPocky <someone-70@live.com>
2021-07-26 13:15:45 -0700

    Added details route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,7 +14,20 @@ router.get("/login", async (req, res) => {
 
 // Lily
 router.get("/details/:id", withAuth, async (req, res) => {
-  res.render("details");
+  try {
+    const projectData = await Project.findByPk(req.params.id);
+    if (!projectData) {
+      res.sendStatus(404).json("Project Not Found");
+      return;
+    }
+    const project = projectData.get({ plain: true });
+    res.render("details", {
+      project,
+      logged_in: req.session.logged_in,
+    });
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
 // Marko
