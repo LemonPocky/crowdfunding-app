177889616c01a209445d1b07bedb1eaff2c78135
LemonPocky <someone-70@live.com>
2021-07-26 13:42:14 -0700

    Added parameters to details.handlebars; added format_date helper to helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,17 @@
-module.exports ={
-    
-}
+module.exports = {
+  // The custom helper 'format_date' takes in a timestamp
+  format_date: (date) => {
+    // use JavaScript Date methods to get month, date, and year
+
+    // add 1 to month because JavaScript uses a zero-based value for the month (January -> 0)
+    const month = date.getMonth() + 1;
+
+    const day = date.getDay();
+
+    // Add 5 years to get the end date for the exhibition
+    const year = date.getFullYear() + 5;
+
+    // Return a string formatted as M/D/YYYY
+    return `${month}/${day}/${year}`;
+  },
+};
