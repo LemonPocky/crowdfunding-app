a73b202e02a8055691b3b78aea123c29d3dd2ffe
LemonPocky <someone-70@live.com>
2021-07-26 13:07:39 -0700

    Fixed route names, fixed details in wrong file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,29 +1,26 @@
-
 //TODO: Implement home routes for serving html pages
-const router = require('express').Router();
-const { User, Project } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, Project } = require("../models");
+const withAuth = require("../utils/auth");
 
 // Israel
-router.get('/', async (req, res) => {
-  res.render('homepage');
+router.get("/", async (req, res) => {
+  res.render("homepage");
 });
 
-router.get('login', async (req, res) => {
-  res.render('login');
+router.get("/login", async (req, res) => {
+  res.render("login");
 });
 
 // Lily
-router.get('details/:id', withAuth, async (req, res) => {
-  res.render('details');
+router.get("/details/:id", withAuth, async (req, res) => {
+  res.render("details");
 });
 
 // Marko
-router.get('profile', withAuth, async (req, res) => {
+router.get("/profile", withAuth, async (req, res) => {
   // TODO: Get the user_id from req.session
-  res.render('profile');
+  res.render("profile");
 });
-
-
 
 module.exports = router;
